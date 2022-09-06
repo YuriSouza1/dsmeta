c57ee097487fa2a957c1fb991791e85e3e8e6403
Yuri Souza <12202193@aluno.cotemig.com.br>
2022-09-06 10:25:13 -0300

    Fist Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components"
 
 
 function App() {
   return (
+    <>
     <h1>Olá mundo</h1>
+    <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/index.tsx
@@ -0,0 +1,10 @@
+import icon from '../assets/img/Notification_icon.svg'
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+                <img src="{icon}" alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
